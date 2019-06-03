268725a60357c87c1fa9629e795b17505ed40317
Alexandr Netsev <netsev@yandex.ru>
2019-06-04 00:34:53 +0500

    Extract scene controller to class.

--- src/index.js
+++ src/index.js
@@ -13,129 +13,14 @@ import "./images/html.png";
 import "./images/javascript.png";
 import "./images/webpack.png";
 import "./images/python.png";
-import GameEngine from "./gameEngine";
-import Player from "./playerController";
-import WallObject from "./wallObject";
-import TriggerObject from "./triggerObject";
-import Camera from "./camera";
-import DecorationObject from "./decorationObject";
-
-window.pixiApp;
-function getData(div, dataName) {
-  return div.dataset[dataName];
-}
-
-function getTextureData(div) {
-  return {
-    name: getData(div, "textureName"),
-    frame: getData(div, "textureFrame")
-  };
-}
-
-function getOptions(div) {
-  const optionsStr = getData(div, "options") || "{}";
-  try {
-    return JSON.parse(optionsStr);
-  } catch (e) {
-    console.warn(
-      `ElementID: "${div.id}" Classes: "${div.classList}"`,
-      `[data-options] is not valid JSON string: ${optionsStr}`
-    );
-    return {};
-  }
-}
-
-function setDivTitle(div) {
-  div.title = div.id + " " + Array(...div.classList).join(" ");
-  div.classList.add("object");
-}
-
-function getAllElements(selector) {
-  return Array.from(document.querySelectorAll(selector));
-    // .filter(node => node.nodeName.toLowerCase() === "div");
-}
-
-function createPlayer() {
-  const playerDiv = getAllElements("[data-scene-player]")[0];
-  const playerSize = playerDiv.getBoundingClientRect();
-  const playerOptions = getOptions(playerDiv);
-  const player = new Player(
-    playerSize.width,
-    playerSize.height,
-    undefined,
-    playerDiv,
-    playerOptions
-  );
-  player.setPosition(playerSize.x, playerSize.y);
-  window.pixiApp.addToScene(player);
-  window.camera.setTarget(player);
-}
-
-function initScene() {
-  createPlayer();
-
-  const decorations = getAllElements("[data-scene-decoration]");
-  decorations.forEach(div => {
-    setDivTitle(div);
-    const size = div.getBoundingClientRect();
-    const texture = getTextureData(div);
-    const options = getOptions(div);
-    const wall = new DecorationObject(
-      size.width, size.height, pixiApp.getTexture(texture), div, options
-    );
-    wall.setPosition(size.x, size.y);
-    window.pixiApp.addToScene(wall);
-  });
-
-  const walls = getAllElements("[data-scene-object]");
-  walls.forEach(div => {
-    setDivTitle(div);
-    const size = div.getBoundingClientRect();
-    const texture = getTextureData(div);
-    const options = getOptions(div);
-    const wall = new WallObject(
-      size.width, size.height, pixiApp.getTexture(texture), div, options
-    );
-    wall.setPosition(size.x, size.y);
-    window.pixiApp.addToScene(wall);
-  });
-
-  const triggers = getAllElements("[data-scene-trigger]");
-  triggers.forEach(div => {
-    setDivTitle(div);
-    const size = div.getBoundingClientRect();
-    const texture = getTextureData(div);
-    const options = getOptions(div);
-    const wall = new TriggerObject(
-      size.width, size.height, pixiApp.getTexture(texture), div, options
-    );
-    wall.setPosition(size.x, size.y);
-    window.pixiApp.addToScene(wall);
-  });
-}
-
-function startPixi() {
-  const container = document.getElementById("pixi-container");
-  const dimentions = container.getBoundingClientRect();
-  const scene = document.getElementById("dom-container");
-  const sceneDimentions = scene.getBoundingClientRect();
-
-  window.camera = new Camera(sceneDimentions);
-
-  window.pixiApp = new GameEngine(container, dimentions, window.camera);
-
-  const divsWithTextures = getAllElements("[data-texture-name]");
-  const textures = new Set();
-  divsWithTextures.forEach(div => {
-    textures.add(getTextureData(div).name);
-  });
-
-  pixiApp.loadTextures(Array(...textures), initScene);
-}
+import SceneController from "./sceneController";
 
 
 function initDomElements() {
-  startPixi();
+  const container = document.getElementById("pixi-container");
+  const scene = document.getElementById("dom-container");
+
+  new SceneController(scene, container);
 }
 
 window.onload = initDomElements;
---
+++ src/sceneController.js
@@ -0,0 +1,122 @@
+import Camera from "./camera";
+import GameEngine from "./gameEngine";
+import WallObject from "./wallObject";
+import TriggerObject from "./triggerObject";
+import DecorationObject from "./decorationObject";
+import Player from "./playerController";
+
+const TEXTURE_ATTR = "[data-texture-name]";
+const TEXTURE_NAME = "textureName";
+const TEXTURE_FRAME = "textureFrame";
+const OBJECT_ATTR = "[data-scene-object]";
+const TRIGGER_ATTR = "[data-scene-trigger]";
+const DECORATION_ATTR = "[data-scene-decoration]";
+const PLAYER_ATTR = "[data-scene-player]";
+const OPTIONS_ATTR = "options";
+
+export default class SceneController {
+  /**
+   * Creates an instance of SceneController.
+   * @param {HTMLElement} sceneContainer
+   * @param {HTMLElement} canvasContainer
+   * @param {*} options
+   * @memberof SceneController
+   */
+  constructor(sceneContainer, canvasContainer, options = {}) {
+    const sceneDims = sceneContainer.getBoundingClientRect();
+    const canvasDims = canvasContainer.getBoundingClientRect();
+
+    this.sceneContainer = sceneContainer;
+    this.canvasContainer = canvasContainer;
+    this.camera = new Camera(sceneDims);
+    this.gameEngine = new GameEngine(
+      this.canvasContainer, canvasDims, this.camera
+    );
+    
+    this.loadTextures();
+  }
+
+  loadTextures() {
+    const divsWithTextures = this.getElementsByDataAttr(TEXTURE_ATTR);
+    const textures = new Set();
+    divsWithTextures.forEach(div => {
+      textures.add(this.getTextureData(div).name);
+    });
+  
+    this.gameEngine.loadTextures(
+      Array(...textures),
+      this.initScene.bind(this)
+    );
+  }
+
+  initScene() {
+    this.createPlayer();
+    this.initObjects(OBJECT_ATTR, WallObject);
+    this.initObjects(TRIGGER_ATTR, TriggerObject);
+    this.initObjects(DECORATION_ATTR, DecorationObject);
+  }
+
+  createPlayer() {
+    const playerDiv = this.getElementsByDataAttr(PLAYER_ATTR)[0];
+    const playerSize = playerDiv.getBoundingClientRect();
+    const playerOptions = this.getOptions(playerDiv);
+    const player = new Player(
+      playerSize.width,
+      playerSize.height,
+      undefined,
+      playerDiv,
+      playerOptions
+    );
+    player.setPosition(playerSize.x, playerSize.y);
+    this.gameEngine.addToScene(player);
+    this.camera.setTarget(player);
+  }
+
+  initObjects(dataAttr, constructor) {
+    const objects = this.getElementsByDataAttr(dataAttr);
+    objects.forEach(div => {
+      const size = div.getBoundingClientRect();
+      const texture = this.getTextureData(div);
+      const options = this.getOptions(div);
+      const object = new constructor(
+        size.width,
+        size.height,
+        this.gameEngine.getTexture(texture),
+        div,
+        options
+      );
+      object.setPosition(size.x, size.y);
+      this.gameEngine.addToScene(object);
+
+      if (options.debug) setDivTitle(div);
+    });
+  }
+
+  getElementsByDataAttr(dataAttr) {
+    return Array.from(this.sceneContainer.querySelectorAll(dataAttr));
+  }
+
+  getData(div, dataName) {
+    return div.dataset[dataName];
+  }
+  
+  getTextureData(div) {
+    return {
+      name: this.getData(div, TEXTURE_NAME),
+      frame: this.getData(div, TEXTURE_FRAME)
+    };
+  }
+  
+  getOptions(div) {
+    const optionsStr = this.getData(div, OPTIONS_ATTR) || "{}";
+    try {
+      return JSON.parse(optionsStr);
+    } catch (e) {
+      console.warn(
+        `ElementID: "${div.id}" Classes: "${div.classList}"`,
+        `[data-options] is not valid JSON string: ${optionsStr}`
+      );
+      return {};
+    }
+  }
+}
