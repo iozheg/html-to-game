243a4874f626f00624ceedf0053580c5093aee26
Aleksandr Netsev <netsev@yandex.ru>
2019-11-21 01:39:45 +0500

    prepare package

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-import "./styles/style.css";
-import "./styles/walls.css";
-import "./styles/animations.css";
-import "./images/github.png";
-import "./images/background.png";
-import "./images/bg-middle.png";
-import "./images/tileset.png";
-import "./images/props.png";
-import "./images/vue.png";
-import "./images/css.png";
-import "./images/html.png";
-import "./images/javascript.png";
-import "./images/webpack.png";
-import "./images/python.png";
 import SceneController from "./sceneController";
+import EventSystem from './eventSystem';
 
-function initDomElements() {
-  const container = document.getElementById("pixi-container");
-  const scene = document.getElementById("dom-container");
-
-  new SceneController(scene, container);
+export default {
+  SceneController,
+  EventSystem,
 }
-
-window.onload = initDomElements;
